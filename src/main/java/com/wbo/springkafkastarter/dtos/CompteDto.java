445feaa4184f7623be564b6765c6ee06dac2337d
Walid <dw_boubechtoula@esi.dz>
2022-06-09 06:34:13 +0100

    Initial commit

---
+++ src/main/java/com/wbo/springkafkastarter/dtos/CompteDto.java
@@ -0,0 +1,13 @@
+package com.wbo.springkafkastarter.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CompteDto {
+    private long id;
+    private long amout;
+}
---
+++ src/test/java/com/wbo/springkafkastarter/SpringKafkaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbo.springkafkastarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
